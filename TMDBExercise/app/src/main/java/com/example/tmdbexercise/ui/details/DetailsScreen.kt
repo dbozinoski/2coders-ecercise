f282e11198ad81b6498655fa307f0f843e6dc6a9
deanbozhinoski <dean.bozhinoski@philips.com>
2024-10-20 02:02:44 +0200

    Adding scroll to DetailsScreen.kt

--- TMDBExercise/app/src/main/java/com/example/tmdbexercise/ui/details/DetailsScreen.kt
+++ TMDBExercise/app/src/main/java/com/example/tmdbexercise/ui/details/DetailsScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -137,7 +139,10 @@ fun DetailsContent(
         // If movie is available, display its details
         movie?.let { movie ->
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+
             ) {
                 Text(
                     modifier = Modifier
